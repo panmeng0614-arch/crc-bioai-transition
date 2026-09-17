cdda79a5248fc155e068d021a6e0812cf452b33e
Meng Pan <panmeng0614@gmail.com>
2026-09-17 21:51:05 +0800

    Add initial Python and pandas practice

---
+++ src/python_day1.py
@@ -0,0 +1,16 @@
+patient = "P010"
+condition = "Adenoma"
+age = 55
+
+print(patient)
+print(condition)
+print(age)
+
+patients = ["P001","P002","P003"]
+print(patients)
+print(patients[1])
+
+conditions = ["Normal","Adenoma","crc","advances crc"]
+
+for condition in conditions:
+    print(condition)
